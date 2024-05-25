527b2115d77c18fd53ab725ac03e7df047faaf05
Jerrick Tram <jerricktram8@gmail.com>
2024-05-25 00:32:04 -0400

    fixed indent in init file

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app():
 
     db.init_app(app)
 
-    with app.app_context():a
+    with app.app_context():
         from .routes import bp as main_bp
         app.register_blueprint(main_bp)
         db.create_all()
