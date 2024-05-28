7406fe21a74da8238bae82a6f85d06b03e3bab94
Jerrick Tram <jerricktram8@gmail.com>
2024-05-28 08:20:24 -0400

    made dark mode the default display and changed container color css

--- app/static/js/dark_mode.js
+++ app/static/js/dark_mode.js
@@ -1,9 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Set dark mode by default
     document.body.classList.add('dark-mode');
-
-    const toggleButton = document.getElementById('dark-mode-toggle');
-    toggleButton.addEventListener('click', function() {
-        document.body.classList.toggle('dark-mode');
-    });
 });
